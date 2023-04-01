5ae66750ebdaaf244ec5dbac15c87920cd0ea7d5
Arthur Martins <113495158+estacio-arthur-martins@users.noreply.github.com>
2023-04-01 12:28:33 -0300

    Criado usando o Colaboratory

---
+++ app_residuos/main.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyP5sbkrMIxpe/0/zDs5r1ew",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/estacio-arthur-martins/TBP_pesquisa_base_dados_publico/blob/main/app_residuos/main.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "1X4b8eRsalk5"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
